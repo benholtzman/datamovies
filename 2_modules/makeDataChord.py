b9ade528319552c57e02e16876e7089244e8ab5d
benholtzman <benholtzman@users.noreply.github.com>
2019-02-14 17:31:04 -0500

    adding new module

--- 2_modules/makeDataChord.py
+++ 2_modules/makeDataChord.py
@@ -7,7 +7,9 @@ def makeDataChord(mNotes,time,times,notes):
     for i,note in enumerate(mNotes):
         a = []
         a = times[notes==note]
+        # find the index at this point. 
         print(a)
+        
         if len(a)%2==0:
             print('even')
             for i in range(len(a)):
@@ -16,11 +18,12 @@ def makeDataChord(mNotes,time,times,notes):
                     ch_notes.append(note)
                     ch_times.append(a[i])
                     ch_durs.append(dur_a)
+                    
         elif len(a)%2==1:
             print('odd')
             for i in range(len(a)):
-                if i<len(a)-1:
-                    if (i)%2==0:
+                if i<len(a)-1:  #  no -1, it breaks; -1 gives same as -2 
+                    if (i)%2==0: 
                         #dur_a = a[i+1]-a[i]
                         dur_a = a[i+1]-a[i]
                         ch_notes.append(note)
